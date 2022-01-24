5944d5e9392e0b5e11793cd7d1535b6b4f3aecb1
Royss Hugo Martel Massi <you@example.com>
2022-01-24 14:41:30 -0500

    colorpicker_distribution

--- app/src/main/java/softmediaconsultores/com/prueba2/ColorAdapter.kt
+++ app/src/main/java/softmediaconsultores/com/prueba2/ColorAdapter.kt
@@ -1,6 +1,7 @@
 package softmediaconsultores.com.prueba2
 
 import android.content.Context
+import android.graphics.BlendModeColorFilter
 import android.graphics.drawable.Drawable
 import android.view.LayoutInflater
 import android.view.View
@@ -11,10 +12,12 @@ import kotlinx.android.synthetic.main.color.view.*
 class ColorAdapter(private val mContext: Context, private val listacolor: List<Color>): ArrayAdapter<Color>(mContext,0,listacolor) {
 
     override fun getView(position: Int, convertView: View?, parent: ViewGroup): View {
+
         val layout = LayoutInflater.from(mContext).inflate(R.layout.color,parent,false)
         val color = listacolor[position]
-
-        layout.incolor.setImageResource(color.imegen)
+        val circulo = layout.findViewById<View>(R.id.incolor)
+        val bordes = layout.findViewById<View>(R.id.incolor2)
+        circulo?.background?.setTint(android.graphics.Color.parseColor(color.colorhex))
         return layout
     }
 }
--- app/src/main/java/softmediaconsultores/com/prueba2/MainActivity.kt
+++ app/src/main/java/softmediaconsultores/com/prueba2/MainActivity.kt
@@ -7,25 +7,49 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
 import android.widget.ImageView
+import androidx.databinding.DataBindingUtil
 import kotlinx.android.synthetic.main.activity_main.*
+import softmediaconsultores.com.prueba2.databinding.ActivityMainBinding
 import kotlin.random.Random
 
 class MainActivity : AppCompatActivity() {
-
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
 
-        listar()
+        inicio()
+    }
+
+    private fun inicio(){
+        binding.btnMostrar.setOnClickListener {
+            listar() }
     }
     private fun listar(){
-        val circulo = R.drawable.item
 
-        val color = Color("#ffffff", circulo)
-
-        val listaColores = listOf(color)
+        val listaColores = generarLista()
 
         val adapter = ColorAdapter(this, listaColores)
-        lista.adapter = adapter
+        binding.lista.adapter=adapter
+    }
+
+    private fun generarLista(): List<Color>{
+        val circulo = R.drawable.item
+        return listOf(
+            Color("#e02020",circulo),
+            Color("#703735",circulo),
+            Color("#d47cb7",circulo),
+            Color("#ba4343",circulo),
+            Color("#6e3e3e",circulo),
+            Color("#a66438",circulo),
+            Color("#ff9a9a",circulo),
+            Color("#aeaeae",circulo),
+            Color("#870a3d",circulo),
+            Color("#a96363",circulo),
+            Color("#c01c67",circulo),
+            Color("#eaa9a9",circulo),
+            Color("#896161",circulo),
+            Color("#cd9898",circulo)
+        )
     }
 }
